6441a48d8b5f6cd7844f078571c0f987fbfd1bc6
Eric Koslow <ekoslow@gmail.com>
2015-10-08 11:51:23 -0700

    [FIX] Add missing case statement in reducer
    Forgot to merge the result for retrievePaths action result.

--- src/reducer.js
+++ src/reducer.js
@@ -1,11 +1,12 @@
 import merge from 'deepmerge';
-import { RETRIEVE_PATH, SET_PATH } from './actions';
+import { RETRIEVE_PATH, RETRIEVE_PATHS, SET_PATH } from './actions';
 
 const initialState = {};
 
 export default function falcorReducer(state = initialState, action) {
   switch (action.type) {
   case RETRIEVE_PATH:
+  case RETRIEVE_PATHS:
   case SET_PATH:
     return merge(state, action.res.json);
   default:
--- test/reducer.spec.js
+++ test/reducer.spec.js
@@ -1,6 +1,6 @@
 import expect from 'expect';
 import falcorReducer from '../src/reducer';
-import { RETRIEVE_PATH } from '../src/actions';
+import { RETRIEVE_PATH, SET_PATH, RETRIEVE_PATHS } from '../src/actions';
 
 describe('falcorReducer', () => {
   describe(RETRIEVE_PATH, () => {
@@ -26,6 +26,76 @@ describe('falcorReducer', () => {
 
       const action = {
         type: RETRIEVE_PATH,
+        res: {
+          json: {
+            foo: 'baz',
+          },
+        },
+      };
+
+      const newState = falcorReducer(state, action);
+      expect(newState).toEqual({foo: 'baz', a: 'b'});
+    });
+  });
+
+  describe(SET_PATH, () => {
+    it('merges the result properly into an inital state', () => {
+      const action = {
+        type: SET_PATH,
+        res: {
+          json: {
+            foo: 'bar',
+          },
+        },
+      };
+
+      const newState = falcorReducer(undefined, action);
+      expect(newState).toEqual({foo: 'bar'});
+    });
+
+    it('merges the result properly into an existing state', () => {
+      const state = {
+        foo: 'bar',
+        a: 'b',
+      };
+
+      const action = {
+        type: SET_PATH,
+        res: {
+          json: {
+            foo: 'baz',
+          },
+        },
+      };
+
+      const newState = falcorReducer(state, action);
+      expect(newState).toEqual({foo: 'baz', a: 'b'});
+    });
+  });
+
+  describe(RETRIEVE_PATHS, () => {
+    it('merges the result properly into an inital state', () => {
+      const action = {
+        type: RETRIEVE_PATHS,
+        res: {
+          json: {
+            foo: 'bar',
+          },
+        },
+      };
+
+      const newState = falcorReducer(undefined, action);
+      expect(newState).toEqual({foo: 'bar'});
+    });
+
+    it('merges the result properly into an existing state', () => {
+      const state = {
+        foo: 'bar',
+        a: 'b',
+      };
+
+      const action = {
+        type: RETRIEVE_PATHS,
         res: {
           json: {
             foo: 'baz',
