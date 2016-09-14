f9831ad255c4adf6ef02b15e6a3c0068336a20c4
J. R. Pickhardt <pickhardt@gmail.com>
2016-09-13 22:10:40 -0700

    Add empty test to tests

---
+++ test/test_redux_falcor.js
@@ -0,0 +1,14 @@
+// Run these tests with `npm test`
+
+import assert from 'assert';
+import { FalcorProvider } from '../src/index';
+import { Model } from 'falcor';
+import React, { Component } from 'react';
+
+const falcorProvider = <FalcorProvider></FalcorProvider>;
+
+describe('TODO', function() {
+  it('should pass', function() {
+    assert.equal(true, true);
+  });
+});
