7af18ac73c7760ac1ea236bd0090140a85671e7c
J. R. Pickhardt <pickhardt@gmail.com>
2016-10-02 23:08:37 -0700

    More duck.js tests

--- test/test_duck.js
+++ test/test_duck.js
@@ -1,18 +1,50 @@
 import assert from 'assert';
-import { update } from '../src/components/duck.js';
+import reduxFalcorReducer, { update } from '../src/components/duck.js';
 
 describe('duck', function() {
-  it('update action has the right type', function() {
-    const action = update();
-    assert.equal('redux-falcor/UPDATE', action.type);
+  describe('update', function() {
+    it('update action has the right type', function() {
+      const action = update();
+      assert.equal('redux-falcor/UPDATE', action.type);
+    });
+  
+    it('update action has the payload', function() {
+      const cache = {
+        name: 'Luke Skywalker',
+        movie: 'Star Wars'
+      };
+      const action = update(cache);
+      assert.equal(action.payload, cache);
+    });    
   });
   
-  it('update action has the payload', function() {
-    const cache = {
-      name: 'Luke Skywalker',
-      movie: 'Star Wars'
-    };
-    const action = update(cache);
-    assert.equal(action.payload, cache);
+  describe('reduxFalcorReducer', function() {
+    describe('default action', function() {
+      it('should have type', function() {
+        assert.throws(() => {
+          reduxFalcorReducer();
+        });
+      });
+
+      it('should initialize state', function() {
+        const state = reduxFalcorReducer(undefined, { type: 'INIT' });
+        assert.deepEqual(state, {});
+      });
+    });
+
+    describe('Update action', function() {
+      it('Update action returns payload', function() {
+        const payload = {
+          name: 'Luke Skywalker',
+          movie: 'Star Wars'
+        };
+        const action = {
+          type: 'redux-falcor/UPDATE',
+          payload
+        };
+        const state = reduxFalcorReducer(undefined, action);
+        assert.deepEqual(state, payload);
+      });
+    });
   });
 });
